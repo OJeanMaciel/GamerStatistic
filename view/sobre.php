71b1e64c8ff10b295bc1f496e15e0357f58e926b
Jean Naciel <jeansud@hotmail.com>
2021-06-23 22:06:43 -0300

    commit

---
+++ view/sobre.php
@@ -0,0 +1,48 @@
+<!Doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../css/sobre.css">
+        <title>Sobre Nós</title>
+    </head>
+    <body> 
+          
+        <section class="sobre" >
+            <div class="container-fluid text-center margin sobre">
+                <h3 class="titlesobre">Sobre nós</h3>
+
+                <div class="container">
+                    <div class="row">
+                        <ul class="letrasobre">
+                            <li>   
+                            <p>Esse site foi desenvolvido pelos alunos<br/>                                                    
+                            Felipe Padilha, <br/> Fernando Augusto, <br/> Jean Maciel, <br/>
+                            Somos alunos da: Universidade de Caxias do Sul - UCS <br/>
+                            Orientados pela professora: Gabriele Dani Bonetti
+                        </p>                        
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section >
+            <div class="container-fluid text-center margin sobre">
+                <h3 class="titlesobre">Nosso objetivo</h3>
+
+                <div class="container">
+                    <div class="row">
+                        <ul class="letrasobre">                        
+                            <p>O nosso site, intitulado “Gamer Statistic”, é uma plataforma desenvolvida para auxiliar revendedores de jogos eletrônicos no direcionamento de suas vendas, servindo como uma ferramenta de análise do mercado de games.
+                            O principal objetivo do site Gamer Statistic, é servir como uma ferramenta na formulação de estratégias de business e marketing para empresas revendedoras de jogos, onde podem ser tomadas decisões importantes no direcionamento dos esforços de negócio.                                                      
+                            </p>                            
+                    </div>
+                </div>                
+            <a  class="btn" href="./index.php">Home</a>
+            </div>
+        </section>
+    </body>
+
+</html>
